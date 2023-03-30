f3c7c2f46a8a9f21e4d8a865371102adf6f33f7d
tyfann <1342344773@qq.com>
2023-03-30 12:59:30 +0200

    runnable version add.

--- README.md
+++ README.md
@@ -1,13 +1,5 @@
 # allRank : Learning to Rank in PyTorch
 
-## About
-
-allRank is a PyTorch-based framework for training neural Learning-to-Rank (LTR) models, featuring implementations of:
-* common pointwise, pairwise and listwise loss functions
-* fully connected and Transformer-like scoring functions
-* commonly used evaluation metrics like Normalized Discounted Cumulative Gain (NDCG) and Mean Reciprocal Rank (MRR)
-* click-models for experiments on simulated click-through data
-
 ### Motivation
 
 allRank provides an easy and flexible way to experiment with various LTR neural network models and loss functions.
@@ -17,65 +9,23 @@ We hope that allRank will facilitate both research in neural LTR and its industr
 ## Features
 
 ### Implemented loss functions:
- 1. ListNet (for binary and graded relevance)
- 2. ListMLE
- 3. RankNet
- 4. Ordinal loss
- 5. LambdaRank
- 6. LambdaLoss
- 7. ApproxNDCG
- 8. RMSE
- 9. NeuralNDCG (introduced in https://arxiv.org/pdf/2102.07831)
+ 1. RankNet
+ 2. ApproxNDCG
+ 3. RMSE
+ 4. NeuralNDCG (introduced in https://arxiv.org/pdf/2102.07831)
 
 ### Getting started guide
 
-To help you get started, we provide a ```run_example.sh``` script which generates dummy ranking data in libsvm format and trains
- a Transformer model on the data using provided example ```config.json``` config file. Once you run the script, the dummy data can be found in `dummy_data` directory
- and the results of the experiment in `test_run` directory. To run the example, Docker is required.
+Download dataset and put it into data folder(you need to create that one in the root folder)
 
-### Configuring your model & training
+first run
 
-To train your own model, configure your experiment in ```config.json``` file and run  
-
-```python allrank/main.py --config_file_name allrank/config.json --run_id <the_name_of_your_experiment> --job_dir <the_place_to_save_results>```
-
-All the hyperparameters of the training procedure: i.e. model defintion, data location, loss and metrics used, training hyperparametrs etc. are controlled
-by the ```config.json``` file. We provide a template file ```config_template.json``` where supported attributes, their meaning and possible values are explained.
- Note that following MSLR-WEB30K convention, your libsvm file with training data should be named `train.txt`. You can specify the name of the validation dataset 
- (eg. valid or test) in the config. Results will be saved under the path ```<job_dir>/results/<run_id>```
- 
-Google Cloud Storage is supported in allRank as a place for data and job results.
-
-
-### Implementing custom loss functions
-
-To experiment with your own custom loss, you need to implement a function that takes two tensors (model prediction and ground truth) as input
- and put it in the `losses` package, making sure it is exposed on a package level.
-To use it in training, simply pass the name (and args, if your loss method has some hyperparameters) of your function in the correct place in the config file:
-
-```
-"loss": {
-    "name": "yourLoss",
-    "args": {
-        "arg1": val1,
-        "arg2: val2
-    }
-  }
+```shell
+pip install -r requirements.txt
 ```
 
-### Applying click-model
+after that run pipeline_test.
 
-To apply a click model you need to first have an allRank model trained.
-Next, run:
-
-```python allrank/rank_and_click.py --input-model-path <path_to_the_model_weights_file> --roles <comma_separated_list_of_ds_roles_to_process e.g. train,valid> --config_file_name allrank/config.json --run_id <the_name_of_your_experiment> --job_dir <the_place_to_save_results>``` 
-
-The model will be used to rank all slates from the dataset specified in config. Next - a click model configured in config will be applied and the resulting click-through dataset will be written under ```<job_dir>/results/<run_id>``` in a libSVM format.
-The path to the results directory may then be used as an input for another allRank model training.
-
-## Continuous integration
-
-You should run `scripts/ci.sh` to verify that code passes style guidelines and unit tests.
 
 ## Research
 
--- pipeline_test.py
+++ pipeline_test.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import json
+import threading
 
 
 # Get the name of the config folder from the user
@@ -14,6 +15,13 @@ def config_generate():
         config_files = [f for f in os.listdir(config_folder) if
                         f.startswith('config_{}'.format(loss)) and f.endswith('.json')]
         for config_file in config_files:
+            task = config_file.split('_')[2]
+            if task == "mq2008":
+                basic_value = "C:\\Users\\tyfann\\Documents\\TU Delft\\IN4325 Information Retrieval\\IN4325-IR\\data\\MQ2008\\{}"
+
+            else:
+                basic_value = "C:\\Users\\tyfann\\Documents\\TU Delft\\IN4325 Information Retrieval\\IN4325-IR\\data\\MSLR-WEB10K\\{}"
+
             config_file_path = os.path.join(config_folder, config_file)
             # 读取原始JSON文件
             with open(config_file_path, "r") as f:
@@ -23,9 +31,19 @@ def config_generate():
             fold_names = ["{}{}_normalized".format(fold_common_name, i) for i in range(1, 6, 1)]
             print(fold_names)
             for fold_name in fold_names:
-                value = "C:\\Users\\tyfann\\Documents\\TU Delft\\IN4325 Information Retrieval\\IN4325-IR\\data\\MSLR-WEB10K\\{}".format(
+                value = basic_value.format(
                     fold_name)
                 config["data"]["path"] = value
+                config["training"]["epochs"] = 30
+                config["training"]["early_stopping_patience"] = 30
+                if "epoch" in config["training"]:
+                    del config["training"]["epoch"]
+                config["val_metric"] = "ndcg_5"
+                config["metrics"] = ["ndcg_5","ndcg_10","ndcg_30","ndcg_60","dcg_5","dcg_10","dcg_30","dcg_60", "mrr_5","mrr_10","mrr_30","mrr_60"]
+                if "mrr_5" in config["expected_metrics"]["val"]:
+                    val = config["expected_metrics"]["val"].pop("mrr_5")
+                    config["expected_metrics"]["val"]["ndcg_5"] = val
+
                 new_config_file = config_file.replace(config_file.split("_")[3].split(".")[0], fold_name.split("_")[0].lower())
 
                 new_filename = os.path.join(config_folder, new_config_file)
@@ -33,11 +51,57 @@ def config_generate():
                     json.dump(config, f, indent=4)
 
 
-if __name__ == '__main__':
-    config_generate()
-    exit(0)
-    config_folder = "allrank\configs"
+def dcg_config_generate():
     loss_group = ["approxndcg", "neuralndcg", "ranknet", "mse"]
+    # Loop through the config files
+    for loss in loss_group:
+        config_folder = "allrank\configs"
+        config_files = [f for f in os.listdir(config_folder) if
+                        f.startswith('config_{}'.format(loss)) and f.endswith('.json')]
+        for config_file in config_files:
+            task = config_file.split('_')[2]
+            if task == "mq2008":
+                basic_value = "C:\\Users\\tyfann\\Documents\\TU Delft\\IN4325 Information Retrieval\\IN4325-IR\\data\\MQ2008\\{}"
+
+            else:
+                basic_value = "C:\\Users\\tyfann\\Documents\\TU Delft\\IN4325 Information Retrieval\\IN4325-IR\\data\\MSLR-WEB10K\\{}"
+
+            config_file_path = os.path.join(config_folder, config_file)
+            # 读取原始JSON文件
+            with open(config_file_path, "r") as f:
+                config = json.load(f)
+
+            fold_common_name = "Fold"
+            fold_names = ["{}{}_normalized".format(fold_common_name, i) for i in range(1, 6, 1)]
+            print(fold_names)
+            for fold_name in fold_names:
+                value = basic_value.format(
+                    fold_name)
+                config["data"]["path"] = value
+                config["training"]["epochs"] = 30
+                config["training"]["early_stopping_patience"] = 30
+                if "epoch" in config["training"]:
+                    del config["training"]["epoch"]
+                config["val_metric"] = "dcg_5"
+                config["metrics"] = ["dcg_5","dcg_10","dcg_30","dcg_60"]
+                if "mrr_5" in config["expected_metrics"]["val"]:
+                    val = config["expected_metrics"]["val"].pop("mrr_5")
+                    config["expected_metrics"]["val"]["dcg_5"] = val
+
+                new_config_file = config_file.replace(config_file.split("_")[3].split(".")[0], fold_name.split("_")[0].lower())
+
+                new_filename = os.path.join(config_folder, new_config_file)
+                with open(new_filename, "w") as f:
+                    json.dump(config, f, indent=4)
+
+if __name__ == '__main__':
+    # config_generate()
+    # exit(0)
+
+    threads = []
+    config_folder = "allrank\configs"
+    # loss_group = ["approxndcg", "neuralndcg", "ranknet", "mse"]
+    loss_group = ["mse"]
     # Loop through the config files
     for loss in loss_group:
         # Build the path to the config file
@@ -48,13 +112,30 @@ if __name__ == '__main__':
         for config_file_name in config_files:
             task_name = config_file_name.split("_")[2]
             fold_name = config_file_name.split("_")[3].split(".")[0]
-            run_id = "test_run_{}".format(loss)
-            job_dir = os.path.join(r"allrank\task-{}".format(task_name), "{}-lr0001-{}".format(loss, fold_name))
-            print(
-                "python allrank\main.py --config-file-name {} --run-id {} --job-dir {}".format(config_file_name, run_id,
-                                                                                               job_dir))
+
+            if not task_name == "web10":
+                continue
+            else:
+                run_id = "test_run_{}".format(loss)
+                job_dir = os.path.join(r"allrank\task-{}".format(task_name), "{}-lr0001-{}-ndcg".format(loss, fold_name))
+
+                # if
+                config_file_name = os.path.join(config_folder, config_file_name)
+                # print(
+                #     "python allrank\main.py --config-file-name {} --run-id {} --job-dir {}".format(config_file_name, run_id,
+                #                                                                                    job_dir))
+                t = threading.Thread(target=os.system("python allrank\main.py --config-file-name {} --run-id {} --job-dir {}".format(config_file_name, run_id, job_dir)))
+                threads.append(t)
+
             # os.system("python allrank\main.py --config-file-name {} --run-id {} --job-dir {}".format(config_file_name, run_id, job_dir))
-        break
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+
     # # Extract the file number
     # file_number = str(i)
     # file_number = file_number.replace("config", "")
